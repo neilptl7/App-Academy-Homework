e5bd98547ae1139d8326973c944a1520262cf455
Neil <neilptl7@gmail.com>
2017-08-03 21:33:19 -0700

    Created Queue Class

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -20,3 +20,26 @@ class Stack
   end
 
 end
+
+#Exercise 2
+
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+    el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue.dup
+  end
+
+end
